218308e52a8df13876a2e20d50797aa9b1336776
ajaiveersandhu <ajaiveersandhu@gmail.com>
2020-10-22 01:12:05 -0400

    Update updateInventory.php

--- updateInventory.php
+++ updateInventory.php
@@ -23,23 +23,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$paymentMethod = mysqli_real_escape_string($mysqli, $_POST['paymentMethod']);
 
 		$book_id = $_SESSION["book_id"];
-		echo "Book id : " . $book_id;
 
 		$q1 = "INSERT INTO `orders` (`firstName`, `lastName`, `book_id`) VALUES ('$firstName', '$lastName', '$book_id')";
 		$newQuantity = (int)$_SESSION['inventory'] - 1;
-		$q2 = "UPDATE `bookinventory` SET `inventory` = $newQuantity WHERE book_id = '$book_id'}";
+		$q2 = "UPDATE `bookinventory` SET `inventory` = '$newQuantity' WHERE book_id = '$book_id'}";
 		$r1 = @mysqli_query($mysqli, $q1);
 		$r2 = @mysqli_query($mysqli, $q2);
 
-		$break = "<br>";
-
-		echo $q1 . $break . $q2 . $break . $r1 . $break . $r2;
 
 		if (mysqli_affected_rows($mysqli)) {
 			$_SESSION = [];
 			session_destroy();
 
-			header("Location: store.php");
+			// header("Location: store.php");
 			// mysqli_free_result($r1);
 		}
 	}
