47b3d1c8d18f259e3e336b30591bf9dbb2761271
ajaiveersandhu <ajaiveersandhu@gmail.com>
2020-10-21 21:27:41 -0400

    index page created

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+	<link rel="stylesheet" href="./styles/main.css">
+	<title>Project 1 | Bookstore</title>
+</head>
+
+<body>
+	<nav class="red lighten-2 z-depth-3">
+		<div class="container red lighten-2">
+			<div class="nav-wrapper">
+				<a href="#" class="brand-logo">| BookStore |</a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href="index.php">Home</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<div class="row two-containers">
+			<img class="responsive-img bookstore_img " src="./images/bookstore.jpg">
+			<div class="about-text">
+				<h2>Vintage BookStore</h2>
+				<p class="flow-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolores ab iusto facilis repudiandae accusamus veritatis magni numquam perspiciatis voluptas recusandae laboriosam quia illo, quos fugit eaque minima optio nulla necessitatibus? Necessitatibus facere maxime pariatur architecto consequuntur assumenda magnam illo.</p>
+				<p class="flow-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis sequi ducimus ullam corrupti in maxime iste neque at perferendis rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto obcaecati aspernatur porro ex quisquam, laborum consectetur deserunt, velit quis error.</p>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
