f59f2396a011bef331976daa2b14344e054f425f
ajaiveersandhu <ajaiveersandhu@gmail.com>
2020-10-22 01:12:44 -0400

    Update updateInventory.php

--- updateInventory.php
+++ updateInventory.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$_SESSION = [];
 			session_destroy();
 
-			// header("Location: store.php");
+			header("Location: store.php");
 			// mysqli_free_result($r1);
 		}
 	}
